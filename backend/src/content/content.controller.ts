4d240a8fb8db7a02fc33c835b2439273d3d7580c
PhilippeMenge <philippemengealima@gmail.com>
2023-08-21 14:00:53 -0300

    1 tentativa de implementar "remover conteúdo"

--- backend/src/content/content.controller.ts
+++ backend/src/content/content.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Param } from '@nestjs/common';
+import { ContentService } from './content.service';
 
 @Controller('content')
-export class ContentController {}
+export class ContentController {
+    constructor(private readonly contentService: ContentService) {}
+
+    @Delete(':id')
+  async removeContent(@Param('id') id: number) {
+    return this.contentService.removeContent(id);
+  }
+}
+
--- backend/src/content/content.service.ts
+++ backend/src/content/content.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class ContentService {}
+export class ContentService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async removeContent(id: number) {
+      const content = await this.prisma.content.findUnique({ where: { id } });
+  
+      if (!content) {
+        throw new NotFoundException(`Content with ID ${id} not found`);
+      }
+  
+      await this.prisma.content.delete({ where: { id } });
+      return { message: `Content with ID ${id} has been removed` };
+    }
+}
