8ece235e3dcd668d79a37e92d3a94656f87b8597
Mader <102488488+MaderG@users.noreply.github.com>
2023-08-21 20:23:08 -0300

    "Test: Add test scenarios for follow/unfollow and followers/following list"

---
+++ backend/src/relationship/relationship.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class RelationshipService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly prisma: PrismaService
+  ) {}
+  
+
+  async followUser(followerId: number, followingId: number): Promise<void> {
+    const isFollowing = await this.prisma.user.findUnique({
+      where: { id: followerId },
+      select: { following: { where: { id: followingId } } },
+    });
+
+    if (!isFollowing) {
+      await this.userService.addFollower(followingId, followerId);
+    }
+    else{
+      await this.userService.removeFollower(followingId, followerId);
+    }
+
+  }
+
+  async isFollowing(followerId: number, followingId: number): Promise<boolean> {
+    const followingUsers = await this.userService.getFollowing(followerId);
+    return followingUsers.some(user => user.id === followingId);
+  }
+}
