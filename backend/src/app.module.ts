b455ba32c5ac0b1f51ca8897f11202e716a1f575
Hbprado <hbp@cin.ufpe.br>
2023-08-21 23:47:51 -0300

    chore: update modules imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,9 @@ import { UserModule } from './user/user.module';
 import { ContentModule } from './content/content.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [UserModule, ContentModule, PrismaModule, AuthModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}), UserModule, ContentModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
