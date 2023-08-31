ffe25dd917beb16a4f19ab78e025d6e615295e89
Hbprado <hbp@cin.ufpe.br>
2023-08-31 15:19:16 -0300

    fix: fixed setupTest.ts

--- setupTests.ts
+++ setupTests.ts
@@ -1,26 +1,30 @@
 import { INestApplication, ValidationPipe } from "@nestjs/common";
 import { PrismaService } from './backend/src/prisma/prisma.service';
 import { AppModule } from './backend/src/app.module';
-import { Test } from "@nestjs/testing"; 
+import { Test, TestingModule } from "@nestjs/testing"; 
+
 let app: INestApplication;
 let prisma: PrismaService;
 
 beforeEach(async () => {
-    // TODO: Add any setup code here (e.g. clear DB for tests before each test case)
-    const moduleRef =
-      await Test.createTestingModule({
+    const moduleFixture: TestingModule = await Test.createTestingModule({
         imports: [AppModule],
       }).compile();
 
-    app = moduleRef.createNestApplication();
+    app = moduleFixture.createNestApplication();
     app.useGlobalPipes(
       new ValidationPipe({
         whitelist: true,
       }),
     );
     await app.init();
-    await app.listen(3333);
 
-    prisma = app.get(PrismaService);
+    prisma = moduleFixture.get<PrismaService>(PrismaService);
     await prisma.cleanDb();
-  });
+});
+
+afterEach(async () => {
+    await app.close();
+});
+
+export { app, prisma };
