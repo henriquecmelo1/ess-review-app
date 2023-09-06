7598394e91159c37646a56c692f25348a68ee97c
Hbprado <hbp@cin.ufpe.br>
2023-09-06 15:19:07 -0300

    fix: remove test script bug

--- setupTests.ts
+++ setupTests.ts
@@ -6,25 +6,4 @@ import { Test, TestingModule } from "@nestjs/testing";
 let app: INestApplication;
 let prisma: PrismaService;
 
-beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-        imports: [AppModule],
-      }).compile();
-
-    app = moduleFixture.createNestApplication();
-    app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true,
-      }),
-    );
-    await app.init();
-
-    prisma = moduleFixture.get<PrismaService>(PrismaService);
-    await prisma.cleanDb();
-});
-
-afterEach(async () => {
-    await app.close();
-});
-
 export { app, prisma };
