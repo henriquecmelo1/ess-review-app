cde28326f9d521e07c333f9b25c00f83d4b5d678
Hbprado <hbp@cin.ufpe.br>
2023-09-06 15:18:22 -0300

    feat: add create_content tests with pactum

--- tests/controllers/create_content.controller.steps.ts
+++ tests/controllers/create_content.controller.steps.ts
@@ -1,108 +1,98 @@
+import { INestApplication, ValidationPipe } from "@nestjs/common"
 import { loadFeature, defineFeature } from 'jest-cucumber';
-import { PrismaService } from '../../backend/src/prisma/prisma.service';
-import { mockDeep } from 'jest-mock-extended';
-import { Test, TestingModule } from '@nestjs/testing';
-import { PrismaClient as OriginalPrismaClient } from '@prisma/client';
-import { ContentService } from '../../backend/src/content/content.service';
-import { ContentController } from '../../backend/src/content/content.controller';
-import * as request from 'supertest';
 import { AppModule } from '../../backend/src/app.module';
-import { NestFactory } from '@nestjs/core';
+import * as pactum from 'pactum';
+import { PrismaService } from '../../backend/src/prisma/prisma.service';
+import { Test } from "@nestjs/testing";
+import { AuthDto } from "../../backend/src/auth/dto";
+import { CreateContentDto } from "../../backend/src/content/dto";
 
-jest.mock('@prisma/client', () => ({
-    PrismaClient: function () {
-        return mockDeep<OriginalPrismaClient>();
-    },
-}));
-
-const feature = loadFeature('tests/features/test-create_content.feature')
-
+const feature = loadFeature('tests/features/test-create_content.feature');
 defineFeature(feature, (test) => {
-    let contentService: ContentService;
-    let prismaMock: OriginalPrismaClient;
-    let response: request.Response;
-    let app; 
-    beforeAll(async () => {
-        prismaMock = new OriginalPrismaClient();
-        const module: TestingModule = await Test.createTestingModule({
-            controllers: [ContentController],
-            providers: [
-                ContentService,
-                {
-                    provide: PrismaService,
-                    useValue: prismaMock,
-                }
-            ]
-        }).compile();
-        
-        contentService = module.get<ContentService>(ContentService);
+  let app: INestApplication;
+  let prisma: PrismaService;
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile(); 
 
-        app = await NestFactory.create(AppModule); // Criação da instância do app
-        await app.init();
-    });
-    afterAll(async () => {
-        await app.close();
-    });
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
+    await app.init();
+    await app.listen(3000);
 
-    test('Criação de Filme', ({given, when, then, and})=>{
-        let userId: number;
-        let movieInfo: any;
-        given(/^que um usuário autenticado com id "(.*)"$/, (id) =>{
-            userId = id;
-            prismaMock.user.findUnique = jest.fn().mockResolvedValue({id: userId});
+    prisma = app.get(PrismaService)
+    await prisma.cleanDb();
+    pactum.request.setBaseUrl('http://localhost:3000')
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  test('Criação de Filme', ({ given, when, then, and }) => {
+
+    given(/^que um usuário autenticado com id "11"$/, ()=>{
+      const dto: AuthDto = {
+      email: 'rgsa@cin.ufpe.br',
+      password: '123',
+      username: 'renan',
+    }
+      return pactum 
+      .spec()
+      .post('/auth/signup')
+      .withBody(dto)
+      .expectStatus(201)
+      .stores('userAt', 'access_token');
+    })
+    when(
+      /^uma solicitação POST é enviada para "(.*)" com informações válidas sobre um filme \(título: "(.*)", duração: "(.*)", gênero: "(.*)", diretor: "(.*)"\)$/,
+      async () => {
+       const dto: CreateContentDto ={
+        title: 'Oppeinheimer',
+        duration: 120,
+        genre: 'Ficção',
+        director: 'Christopher Nolan',
+       }
+       return pactum
+       .spec()
+       .post('/contents')
+       .withHeaders({
+        Authorization: 'Bearer $S{userAt}' 
         })
-        when(/^uma solicitação POST é enviada para "(.*)" com informações válidas sobre um filme \(título: "(.*)", duração: "(.*)", gênero: "(.*)", diretor: "(.*)"\)$/,
-        async (url, titulo, duracao, genero, diretor)=> {
-            movieInfo = { title: titulo, duration: duracao, genre: genero, director: diretor};
-            prismaMock.content.create = jest.fn().mockResolvedValueOnce({id: '1', ...movieInfo})
-            response = await request(app)
-                .post('/content')
-                .send(movieInfo)
-                .expect(201);
-        });
-        then(/^o status da resposta deve ser "(.*)"$/, (statusCode) => {
-            expect(response.status).toBe(parseInt(statusCode, 10));
-        });
-        and("o corpo da resposta deve conter o filme criado", ()=>{
-            expect(response.body).toEqual({id:'1', ...movieInfo});
-        });
+       .withBody(dto)
+       .expectStatus(201)
+       .stores('contentId', 'id')
+      }
+    );
+    then(/^o status da resposta deve ser "(.*)"$/, (statusCode) => {
+    });
+    and('o corpo da resposta deve conter o filme criado', () => {
+    });
+  });
+
+  test('Obter Conteúdos', ({ given, when, then, and }) => {
+
+    given('que existem conteúdos na base de dados', () => {
+      return pactum
+      .spec()
+      .get('/contents')
+      .withHeaders({
+        Authorization: 'Bearer $S{userAt}',
+      })
+      .expectStatus(200)
+      .expectJsonLength(1);
     });
 
-    test('Obter todos os Conteúdos', ({ given, when, then, and })=>{
-        let contentList: any[];
-        given('que existem conteúdos disponíveis na base de dados', () =>{
-            contentList = [{id: '1', title: 'Interestelar'}, {id: '2', title: 'Barbie'}]
-            prismaMock.content.findMany = jest.fn().mockResolvedValueOnce(contentList);
-        })
-        when(/^uma solicitação GET é enviada para "(.*)"$/,
-         async ()=> {
-            response = await contentService.getContent(1);
-        });
-        then(/^o status da resposta deve ser "(.*)"$/, (statusCode) => {
-            expect(response.status).toBe(200);
-        });
-        and("o corpo da resposta deve conter uma lista de conteúdos", ()=>{
-            expect(response.body).toEqual(contentList);
-        });
+    when(/^uma solicitação GET é enviada para "(.*)"$/, async () => {
     });
-
-    test('Obter Conteúdo por ID', ({ given, when, then, and })=>{
-        let contentId: number;
-        let contentDetails: any;
-        given(/^que um conteúdo com id "(.*)" existe na base de dados$/, (id) =>{
-            contentId = id;
-            contentDetails = {id: contentId, title: 'Interestelar', duration: '120min'}
-            prismaMock.content.findUnique = jest.fn().mockResolvedValueOnce(contentDetails);
-        })
-        when(/^uma solicitação GET é enviada para "(.*)"$/,
-         async ()=> {
-            response = await contentService.getContentById(1,contentId);
-        });
-        then(/^o status da resposta deve ser "(.*)"$/, (statusCode) => {
-            expect(response.status).toBe(200);
-        });
-        and(/^o corpo da resposta deve conter as informações detalhadas do conteúdo com id "(.*)"$/, (id) =>{
-            expect(response.body).toEqual(contentDetails);
-        });
+    then(/^o status da resposta deve ser "(.*)"$/, (statusCode) => {
     });
+    and('o corpo da resposta deve conter as informações detalhadas dos conteúdos', () => {
+    });
+  });
 });
--- tests/features/test-create_content.feature
+++ tests/features/test-create_content.feature
@@ -1,19 +1,14 @@
 Feature: Funcionalidades de Conteúdo (Filmes)
 
     Scenario: Criação de Filme
-        Given que um usuário autenticado com id "789"
+        Given que um usuário autenticado com id "11"
         When uma solicitação POST é enviada para "/content" com informações válidas sobre um filme (título: "Interestelar", duração: "120min", gênero: "Ficção Científica", diretor: "Christopher Nolan")
         Then o status da resposta deve ser "201 Created"
         And o corpo da resposta deve conter o filme criado
 
-    Scenario: Obter todos os Conteúdos
-        Given que existem conteúdos disponíveis na base de dados
+    Scenario: Obter Conteúdos
+        Given que existem conteúdos na base de dados
         When uma solicitação GET é enviada para "/content"
         Then o status da resposta deve ser "200 OK"
-        And o corpo da resposta deve conter uma lista de conteúdos
+        And o corpo da resposta deve conter as informações detalhadas dos conteúdos
 
-    Scenario: Obter Conteúdo por ID
-        Given que um conteúdo com id "987" existe na base de dados
-        When uma solicitação GET é enviada para "/content/987"
-        Then o status da resposta deve ser "200 OK"
-        And o corpo da resposta deve conter as informações detalhadas do conteúdo com id "987"
