c01f0c83be24d8c00f099ca866082f93676aad14
Hbprado <hbp@cin.ufpe.br>
2023-09-12 19:30:03 -0300

    feat: add carousel on home component

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
+  slideConfig = {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+  }
+  slides = [
+    { img: 'assets/movie_1.jpg' },
+    { img: 'assets/movie_2.jpg' },
+    { img: 'assets/movie_3.jpg' },
+    { img: 'assets/movie_4.jpg' },
+    { img: 'assets/movie_5.jpg' },
+    { img: 'assets/movie_6.jpg' },
+    { img: 'assets/movie_7.jpg' },
+  ]
+  constructor(private router: Router){}
+  redirectToSignup(event: Event){
+    event.preventDefault();
+    this.router.navigate(['auth/signup']);
+  }
+  redirectToSignin(event: Event){
+    event.preventDefault();
+    this.router.navigate(['auth/signin']);
+  }
 }
