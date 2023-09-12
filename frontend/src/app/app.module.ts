d79caa51c5c2c8de1f147cfbaa212499c427f42c
Hbprado <hbp@cin.ufpe.br>
2023-09-12 19:26:46 -0300

    feat: update imports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,29 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from  '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from  '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
-import {GaugeModule} from 'angular-gauge';
-import { MatTabsModule} from '@angular/material/tabs';
-import { MatIconModule} from '@angular/material/icon';
-import { MatFormFieldModule} from '@angular/material/form-field';
-import { MatSelectModule} from '@angular/material/select';
+import { GaugeModule } from 'angular-gauge';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { RouterModule } from '@angular/router';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     HomeComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    CarouselComponent,
+    ProfileComponent,
   ],
   imports: [
+    SlickCarouselModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
@@ -34,6 +39,9 @@ import { RouterModule } from '@angular/router';
     MatSelectModule,
     MatTabsModule,
     MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       {
         path:'', component :HomeComponent,
@@ -43,7 +51,7 @@ import { RouterModule } from '@angular/router';
       },
       {
         path: 'auth/signup', component: RegisterComponent
-      }
+      },
     ])
   ],
   providers: [],
