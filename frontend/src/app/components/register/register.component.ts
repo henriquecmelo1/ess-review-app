0e80e8d03e42feeddf31139cfcac8091b61b5396
Hbprado <hbp@cin.ufpe.br>
2023-09-12 19:29:03 -0300

    feat: update register component

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-
+  //form: FormBuilder;
+  constructor(private router: Router, private formBuilder: FormBuilder){}
+  redirectToSignin(event: Event){
+    event.preventDefault();
+    this.router.navigate(['auth/signin']);
+  }
+  /*ngOnInit(): void{
+    this.form = this.formBuilder.group({
+      username: '',
+      email: '',
+      password: '',
+    })
+  }*/
 }
