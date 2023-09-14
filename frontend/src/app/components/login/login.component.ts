59968614a84dffdb4534da70fce60074996b3a63
PhilippeMenge <philippemengealima@gmail.com>
2023-09-13 21:47:06 -0300

    fix: fixing relative paths

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth.service';
-import { Login } from 'src/app/models/login';
-import { JwtAuth } from 'src/app/models/jwtAuth';
+import { AuthService } from '../../services/auth.service';
+import { Login } from '../../models/login';
+import { JwtAuth } from '../../models/jwtAuth';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
