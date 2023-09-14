1dbd1786c492ac8120a2d084ec434cc28fc94202
Hbprado <hbp@cin.ufpe.br>
2023-09-14 11:26:14 -0300

    chore: updating assets

--- frontend/src/app/models/userModel.ts
+++ frontend/src/app/models/userModel.ts
@@ -1,6 +1,12 @@
-export interface User {
+export class User {
     id: number;
     username: string;
     email: string;
     password: string;
+    constructor(user:User) {
+        this.id = user.id;
+        this.username = user.username;
+        this.email = user.email;
+        this.password = user.password;
+    }
   }
